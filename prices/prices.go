a3d27e54a137f1adfa02fbf1da66ba091aa54335
yray akemu <yrayak@outlook.com>
2024-09-28 21:15:03 -0400

    working with file data

--- prices/prices.go
+++ prices/prices.go
@@ -1,6 +1,11 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
@@ -8,12 +13,56 @@ type TaxIncludedPriceJob struct {
 	TaxIncludedPrices map[string]float64
 }
 
+// receiver method to read data from file
+func (job *TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
+
+	if err != nil {
+		fmt.Println("could not open file!!")
+		fmt.Println(err)
+		return
+	}
+	// bufio is used for reading line by line on a file
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+	for scanner.Scan() { // keeps scanning till there is no more content to scan
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err() // find out if an error occured earlier
+	if err != nil {
+		fmt.Println("reading the file content failed!!")
+		fmt.Println(err)
+		file.Close() // close the file
+		return
+	}
+
+	prices := make([]float64, len(lines))
+	for lineIndex, line := range lines {
+		floatPrice, err := strconv.ParseFloat(line, 64) // converts text to other value types
+
+		if err != nil {
+			fmt.Println("converting price to float failed!!")
+			fmt.Println(err)
+			file.Close()
+			return
+		}
+
+		prices[lineIndex] = floatPrice
+	}
+
+	job.InputPrices = prices
+}
+
 // adding a receiver method
-func (job TaxIncludedPriceJob) Process() {
-	result := make(map[string]float64)
+func (job *TaxIncludedPriceJob) Process() {
+	job.LoadData()
+
+	result := make(map[string]string)
 
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 
 	fmt.Println(result)
