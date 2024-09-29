7ef5256065647d721a43e13ddaafc2d7849b0c86
yray akemu <yrayak@outlook.com>
2024-09-28 20:57:44 -0400

    added new package `prices` and updated prices with a constructor and a receiver method

--- main.go
+++ main.go
@@ -1,22 +1,12 @@
 package main
 
-import "fmt"
+import "unsafemango.com/price-calculator/prices"
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64) // create an empty map value
-
 	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
-
-	fmt.Println(result)
 }
